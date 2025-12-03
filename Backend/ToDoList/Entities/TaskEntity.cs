62736fdde94b9c2003678e539ca44549d6eb8b71
Numan Maarij <numanmaarij21@gmail.com>
2025-12-03 10:51:39 +0000

    Docker compose file for PostgreSQL created & Entity classes created

---
+++ Backend/ToDoList/Entities/TaskEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class TaskEnttiy
+{
+    public int Id { get; set; };
+    public int UserId { get; set; }; // FK
+    public string Header { get; set; };
+    public string Body { get; set; };
+    public bool IsComplete { get; set; };
+    public bool IsCancelled { get; set; };
+    public DateOnly CompleteTargetDate { get; set; };
+}
