c3a37a57e8a75ed310c4a7b11f5e0d5b7c503956
Numan Maarij <numanmaarij21@gmail.com>
2025-12-03 11:54:03 +0000

    Fixed namespace issue & model classes

---
+++ Backend/ToDoList/ToDoList.Api/Models/TaskEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList.Api.Models;
+
+public class TaskEntity
+{
+    public int Id { get; set; }
+    public UserEntity UserId { get; set; } = null!;
+    public string Header { get; set; } = string.Empty;
+    public string? Body { get; set; }
+    public bool IsComplete { get; set; }
+    public bool IsCancelled { get; set; }
+    public DateOnly? CompleteTargetDate { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Backend/ToDoList/ToDoList.Api/Models/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoList.Api.Models;
+
+
+public class UserEntity
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string? Location { get; set; } // They may choose to not share location
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ICollection<TaskEntity> Tasks { get; set; } = new List<TaskEntity>();
+
+}
