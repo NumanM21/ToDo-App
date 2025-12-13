95dcf6fe1386c78b8d982d826c21ad78dd93288f
Numan Maarij <numanmaarij21@gmail.com>
2025-12-13 01:33:12 +0000

    Created DbContext class. DbSet, onModelCreate mapping for EF and initial migration. Added DI into program class, with DefaultConfiguration also set up in settings.json.

---
+++ Backend/ToDoList/ToDoList.Api/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Api.Data;
+using ToDoList.Api.Models;
+
+public class AppDbContext : DbContext 
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    // DbSet - One per entity - Represents our tables 
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<TaskEntity> Tasks { get; set; }
+    
+    // Config for our relationships/ constrains within each table and between them
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // User
+        modelBuilder.Entity<UserEntity>(entity =>
+        {
+            entity.HasKey(k => k.Id); // PK
+            entity.HasIndex(k => k.Email).IsUnique();
+            entity.Property(k => k.Email).IsRequired();
+            entity.Property(k => k.FirstName).IsRequired();
+            entity.Property(k => k.LastName).IsRequired();
+        });
+        
+        // Task
+
+        modelBuilder.Entity<TaskEntity>(entity =>
+        {
+            entity.HasKey(k => k.Id); // PK
+
+            // Define the 1-to-many relationship
+            entity.HasOne(t => t.User) // TASK has one user - Navigation to User (No column in table)
+                .WithMany(x => x.Tasks) // User has many tasks
+                .HasForeignKey(k => k.UserId) // FK - Scalar prop - creates column in table
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        
+    }
+}
---
+++ Backend/ToDoList/ToDoList.Api/Migrations/20251213013025_InitialCreate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ToDoList.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Header = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: true),
+                    IsComplete = table.Column<bool>(type: "boolean", nullable: false),
+                    IsCancelled = table.Column<bool>(type: "boolean", nullable: false),
+                    CompleteTargetDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
