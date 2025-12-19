01bb05d27407a8d5312cf2e49f917e36b1addc4d
Numan Maarij <numanmaarij21@gmail.com>
2025-12-19 12:22:49 +0000

    Set up React + Vite + Node.js. Configured React to use Tailwind.css instead of default Vite.css. Installed all necessary packages including React Router, Axios, Tailwind.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Main component - Our UI Code
+function App() {  // similar to  // useState - React hook  (function) - returns curr val and function to update it -- state change -> component re-renders (UI)
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // Looks like HTML, but is JSX (React JS)
+    return (
+        <div className="min-h-screen bg-gray-500 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold text-blue-600 mb-4">
+                    Tailwind is working!
+                </h1>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    Click me
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 // Similar to Program.cs
-
+// React bootstrapper 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
