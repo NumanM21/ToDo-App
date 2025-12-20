eb7d9f3d051bb446c849fb700558f6ed5fbe5d32
Numan Maarij <numanmaarij21@gmail.com>
2025-12-20 02:55:57 +0000

    Added Comments to help understand React, Axios, and TS Syntax better.

--- Frontend/services/api.ts
+++ Frontend/services/api.ts
@@ -5,31 +5,41 @@ import {AuthResponse, LoginCredentials, RegisterCredentials, Task} from "../type
 // Base URL for my backend
 const API_BASE_URL = 'http://localhost:5193/api';
 
-// Create Axios instance
+// Create Axios instance // similar ti creating a configured HttpClient in C#
 const api = axios.create({
     baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
+    headers: {  // Default header for every request
+        'Content-Type': 'application/json',  // Tells server we're sending json
     },
 });
-
+// Interceptor - Middleware that runes BEFORE every request (for all outgoing request)
 // Interceptor - add token to every request automatically
 api.interceptors.request.use((config) => {
+    // Config - the request configuration object set up above - gets used in our request
+    
+    // Get JWT from  browser's localStorage -- localStorage.getItem() returns string or null
     const token = localStorage.getItem('token');
     
+    // token = true if user is logged in
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // `` string interpolation  
+        // Add bearer token to our authorisation header on this req  
+        config.headers.Authorization = `Bearer ${token}`; // `` Template Literal - string interpolation  
     }
+    // Get our modified config with token added to header - This gets sent to server
     return config;
 });
-
+// Export makes this accessible  in other files 
 // Authenticate API calls
-export const authAPI = {
-    // If we call a login? it will pass our credentials using our LoginCredentials method from types and it promises to return an AuthResponse which  we map 
+export const authAPI = { // export an object named authAPI containing auth-related functions
+    // Property - Login - Value - Async arrow function - returns a Promise (C# Task but in JS)
     login: async (credentials: LoginCredentials): Promise<AuthResponse> => {
-        // Store the response from our auth/login api endpoint, passing in the credentials as the argument
+        // Credentials - Parameter with type annotation - Promise - return type (resolves to  AuthResponse
+        // Axios automatically JSON.Stringifies the credentials object
+        // api.post - POST request to url with credentials as body
         const response = await api.post('/auth/login', credentials);
-        return response.data; // .data is the axios response field?
+        // Axios automatically JSON.Stringifies the credentials object
+        // response.data retrieves only data from http body
+        return response.data; 
     },
     
     register: async (data: RegisterCredentials): Promise<AuthResponse> =>{
@@ -38,26 +48,30 @@ export const authAPI = {
     },
 };
 
+
+//// Export const TaskAPI = {several properties (methods) in one object -- JS specific -- Similar to static class in C# - TaskAPI is the object - getAll, getById are properties}
+
 // Task API calls
-export const TaskAPI = {
+export const TaskAPI = {   
+    // No param needed - token identifies user (logged in user) - Promise return Array of Tasks
     getAll: async (): Promise<Task[]> => {
         const response = await api.get('/tasks');
         return response.data;
     },
-    // How can we have several of these in one export const? Is export const a function call we make, and depending on which method we call  (getAll) it will match to that key? Then call the method for it?
-    // is async method type, with id:number the parameter we pass into the api.get request (we pass an id, of type number)
+    
     getById: async (id:number): Promise<Task> => {
         const response = await api.get(`/task/${id}`);
         return response.data;
     },
-    // What is the Omit doing here? is that the field we don't want to update/ pass back via our DTO? 
+    // Omit <> - TS Utility type (Task interface (new type created) without the specified fields we put inside) - Backend generates those 3 so we don't need to!
     create: async (task:Omit<Task,'id' | 'userId' | 'createdAt'>): Promise<Task> => {
         const response = await api.post('/task', task);
-        return response.data;
+        return response.data; // Back returns created task with the 3 omitted field!
     },
-    
+    // Partial<> - TS Utility type -- Task interface, but all properties are optional 
     update: async (id:number, task: Partial<Task>): Promise<void> => {
-        await api.put(`/task/${id}`, {id, ...task}); //  we pass a task with id for our task, then based on that task id, we spread out the task field using the ... ?
+        // Body we pass is {id,...task} -- This ... is an object spread syntax (spreads all properties of task object)
+        await api.put(`/task/${id}`, {id, ...task}); 
     },
     
     delete: async (id:number): Promise<void> => {
